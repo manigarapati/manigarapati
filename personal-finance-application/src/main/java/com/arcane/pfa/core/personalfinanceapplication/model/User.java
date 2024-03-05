f5856cdfadfaaaee814261b87565eace1f5b5168
manigarapati <manigarapati007@gmail.com>
2024-03-05 15:01:31 -0500

    personal-finance-application

---
+++ personal-finance-application/src/main/java/com/arcane/pfa/core/personalfinanceapplication/model/User.java
@@ -0,0 +1,80 @@
+package com.arcane.pfa.core.personalfinanceapplication.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private LocalDate dateJoined;
+
+//	public Long getUserId() {
+//		return userId;
+//	}
+//
+//	public void setUserId(Long userId) {
+//		this.userId = userId;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public void setFirstName(String firstName) {
+//		this.firstName = firstName;
+//	}
+//
+//	public String getLastName() {
+//		return lastName;
+//	}
+//
+//	public void setLastName(String lastName) {
+//		this.lastName = lastName;
+//	}
+//
+//	public LocalDate getDateJoined() {
+//		return dateJoined;
+//	}
+//
+//	public void setDateJoined(LocalDate dateJoined) {
+//		this.dateJoined = dateJoined;
+//	}
+
+}
